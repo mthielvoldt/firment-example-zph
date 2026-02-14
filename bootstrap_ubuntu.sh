b3eceb0247a1c7fed555f7c569d65c3d37337834
Mike T <mthielvoldt@gmail.com>
2026-02-14 18:14:13 -0500

    Add generate script, fix bootstrap_ubuntu.sh

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 sudo apt install --no-install-recommends git cmake ninja-build gperf \
   ccache dfu-util device-tree-compiler wget python3-dev python3-venv python3-tk \
   xz-utils file make gcc gcc-multilib g++-multilib libsdl2-dev libmagic1
@@ -11,7 +13,7 @@ else
   source .venv/bin/activate;
 fi
 
-if ls -d "zephyr-sdk-*"; then
+if [ -d zephyr-sdk-* ]; then
   echo "SDK already present.";
 else
   west sdk install -b $(pwd) --toolchains arm-zephyr-eabi;
---
+++ generate.sh
@@ -0,0 +1,11 @@
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "ERROR: This script must be sourced. Run it with: source ${BASH_SOURCE[0]}"
+    exit 1
+fi
+
+# Activate the venv with west installed if not already.
+if [ ! "$VIRTUAL_ENV" ]; then
+  . .venv/bin/activate
+fi
+
+west build -b nucleo_l476rg --build-dir build/nucleo app
